e3b203da2b6d5c851af13fd8b025a11017917e8f
Justin Hall <hall.justin.d@gmail.com>
2019-04-24 00:00:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,7 +73,7 @@ def can_be_found_updated_and_saved
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
-  __
+  movie
 end
 
 def can_update_using_update_method
